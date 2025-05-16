a7e94d8f0bf3d50537ec77c9d3a09a713343eb05
Jameshao <54829185+James-handsome@users.noreply.github.com>
2025-05-16 18:00:15 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -44,6 +44,12 @@ PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
+PACKAGES="$PACKAGES luci-app-store
+PACKAGES="$PACKAGES luci-app-xunyou
+PACKAGES="$PACKAGES luci-app-turboacc
+PACKAGES="$PACKAGES luci-app-lucky
+ 
+
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
